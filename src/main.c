7fba0b3642cfc03aa4b9ce68dfea155b8b167738
Alexey Solomatin <alexey.solomatin@gmail.com>
2016-01-12 17:12:51 +0300

    Added the USB support (in progress).

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <timer.h>
+//#include <timer.h>
 #include "diag/Trace.h"
 
 #include "BlinkLed.h"
@@ -56,39 +56,26 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-int
-main(int argc, char* argv[])
-{
-  // Send a greeting to the trace device (skipped on Release).
-  trace_puts("Hello ARM World!");
+int main(int argc, char* argv[]) {
+	// Send a greeting to the trace device (skipped on Release).
+	trace_puts("Hello ARM World!");
 
-  // At this stage the system clock should have already been configured
-  // at high speed.
-  trace_printf("System clock: %u Hz\n", SystemCoreClock);
+	// At this stage the system clock should have already been configured
+	// at high speed.
+	trace_printf("System clock: %u Hz\n", SystemCoreClock);
 
-  timer_start();
+	//timer_start();
 
-  blink_led_init();
-  
-  SERVO5_Init_on_PB4();
+	//usb_init();
 
-  uint32_t seconds = 0;
+	//blink_led_init();
 
-  // Infinite loop
-  while (1)
-    {
-      blink_led_on();
-      timer_sleep(seconds == 0 ? TIMER_FREQUENCY_HZ : BLINK_ON_TICKS);
+	SERVO5_Init_on_PB4(); // TODO: refactor this library
 
-      blink_led_off();
-      timer_sleep(BLINK_OFF_TICKS);
-
-      ++seconds;
-
-      // Count seconds on the trace device.
-      trace_printf("Second %u\n", seconds);
-    }
-  // Infinite loop, never return.
+	// Infinite loop
+	while (1) {
+	}
+	// Infinite loop, never return.
 }
 
 #pragma GCC diagnostic pop
